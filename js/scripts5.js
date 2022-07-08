e5451b145919d7b1aad6aadac48241a4acddc6b6
shyndick <3gggkostya@gmail.com>
2022-07-08 11:35:42 +0300

    (js5)complied

---
+++ js/scripts5.js
@@ -0,0 +1,14 @@
+let a = 10,
+    b = 2,
+    sum = a + b,
+    dif = a - b,
+    comp = a * b,
+    priv = a / b;
+
+document.write (`Число a = ${a} Число b = ${b}.<br> Сумма ${sum}. Разность ${dif}. Произведение ${comp}. Частное ${priv}<br>`)
+
+if (sum > 1) {
+    document.write('Сумма в квадрате: ' + sum * sum);
+} else{
+    document.write('Сумма < 1');
+}
