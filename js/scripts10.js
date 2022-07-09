1dec8306089b8135fc18755413bddca2d4a4bb92
Shyndick <3gggkostya@gmail.com>
2022-07-09 12:05:37 +0300

    complied

--- js/scripts10.js
+++ js/scripts10.js
@@ -10,74 +10,50 @@ if (day < 365) {
 }
 switch(true) {
     case n >= 0 && n <= 1: 
-        document.write('Январь <br>');
-    case n >= 0 && n <= 2:
-        document.write('Зима');
+        document.write('Январь <br> Зима');
         break;
 
     case n > 1 && n <=2:
-        document.write('Февраль <br>');
-    case n > 0 && n <= 2:
-        document.write('Зима');
+        document.write('Февраль <br> Зима');
         break;
 
     case n > 2 && n <= 3:
-        document.write('Март <br>');
-    case n >= 3 && n <= 5:
-        document.write('Весна');
+        document.write('Март <br> Весна');
         break;
 
     case n > 3 && n <= 4:
-        document.write('Апрель <br>');
-    case n >= 3 && n <= 5:
-        document.write('Весна');
+        document.write('Апрель <br> Весна');
         break;
 
     case n > 4 && n <= 5:
-        document.write('Май <br>');
-    case n >= 3 && n <= 5:
-        document.write('Весна');
+        document.write('Май <br> Весна');
         break;
 
     case n > 5 && n <= 6:
-        document.write('Июнь <br>');
-    case n >= 6 && n <= 8:
-        document.write('Лето');
+        document.write('Июнь <br> Лето');
         break;
 
     case n > 6 && n <= 7:
-        document.write('Июль <br>');
-    case n >= 6 && n <= 8:
-        document.write('Лето');
+        document.write('Июль <br> Лето');
         break;
 
     case n > 7 && n < 8:
-        document.write('Август <br>');
-    case n >= 6 && n <= 8:
-        document.write('Лето');
+        document.write('Август <br> Лето');
         break;
 
     case n > 8 && n <= 9:
-        document.write('Сентябрь <br>');
-    case n >= 9 && n <= 11:
-        document.write('Осень');
+        document.write('Сентябрь <br> Осень');
         break;
 
     case n > 9 && n <= 10:
-        document.write('Октябрь <br>');
-    case n >= 9 && n <= 11:
-        document.write('Осень');
+        document.write('Октябрь <br> Осень')
         break;
 
     case n > 10 && n <= 11:
-        document.write('Ноябрь <br>');
-    case n >= 9 && n <= 11:
-        document.write('Осень');
+        document.write('Ноябрь <br> Осень');
         break;
 
     case n > 11  && n <= 12:
-        document.write('Декабрь <br>');
-    case n > 11 && n <= 12:
-        document.write('Зима');
+        document.write('Декабрь <br> Зима');
         break;
 }
